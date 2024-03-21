e172e63a5614f3d10ec0e5a6a05273a0c8164a37
skmutai6 <skmutai6@gmail.com>
2024-03-22 00:50:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() 
+{
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat()
+{
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+
+
+function appendCat()
+{
+    const appendCat = [...cats, "Broom"];
+}
+
+function prependCat()
+{
+    const prependCat = ["Arnold", ...cats];
+}
+
+function removeLastCat()
+{
+    cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat()
+{
+    cats.slice(1);
+}
